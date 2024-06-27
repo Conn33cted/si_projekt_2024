e111af9b702997478d81e7b54c67594a4d91bebb
Conn3cted <antis324@gmail.com>
2024-06-27 16:50:20 +0200

    Dodanie formatowania dat

---
+++ app/migrations/Version20240627134205.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240627134205 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE guest DROP identifier, DROP last_creation_date, DROP creation_count');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE guest ADD identifier VARCHAR(255) NOT NULL, ADD last_creation_date DATETIME DEFAULT NULL, ADD creation_count INT NOT NULL');
+    }
+}
--- app/migrations/Version20240627142011.php
+++ app/migrations/Version20240627142011.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240617100705 extends AbstractMigration
+final class Version20240627142011 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20240617100705 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE shorten ADD is_blocked TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE guest DROP identifier, DROP last_creation_date, DROP creation_count');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE shorten DROP is_blocked');
+        $this->addSql('ALTER TABLE guest ADD identifier VARCHAR(255) NOT NULL, ADD last_creation_date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, ADD creation_count INT NOT NULL');
     }
 }
